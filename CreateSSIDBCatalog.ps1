27599d65e684710956fb8e7ae43b08688e6cc118
SHOSBITeam <mark.plaskow@shos.com>
2018-09-13 19:09:08 -0500

    *** MEP 09/13/18 - Parameterized All Connection Strings ***

---
+++ CreateSSIDBCatalog.ps1
@@ -0,0 +1,18 @@
+﻿# Load the IntegrationServices Assembly  
+[Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Management.IntegrationServices")  
+
+# Store the IntegrationServices Assembly namespace to avoid typing it every time  
+$ISNamespace = "Microsoft.SqlServer.Management.IntegrationServices"  
+
+Write-Host "Connecting to server ..."  
+
+# Create a connection to the server  
+$sqlConnectionString = "Data Source=AZSQL01;Initial Catalog=master;UID=mssql_etl;PWD=R985%&*#hgt674QW;"  
+$sqlConnection = New-Object System.Data.SqlClient.SqlConnection $sqlConnectionString  
+
+# Create the Integration Services object  
+$integrationServices = New-Object $ISNamespace".IntegrationServices" $sqlConnection  
+
+# Provision a new SSIS Catalog  
+$catalog = New-Object $ISNamespace".Catalog" ($integrationServices, "SSISDB", "R985%&*#hgt674QW")  
+$catalog.Create()  
